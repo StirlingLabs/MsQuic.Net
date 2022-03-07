7a1b09c9a92f355046322f0188604208cd17cceb
Tyler Young <tyler.young@stirlinglabs.com>
2022-03-07 13:03:39 -0500

    add tracing to PinnedDataSendContext

--- StirlingLabs.MsQuic/QuicStream.PinnedDataSendContext.cs
+++ StirlingLabs.MsQuic/QuicStream.PinnedDataSendContext.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Buffers;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Microsoft.Quic;
@@ -18,14 +19,19 @@ public partial class QuicStream
         public unsafe PinnedDataSendContext(MemoryHandle dataHandle, QUIC_BUFFER* quicBuffer, TaskCompletionSource<bool> taskCompletionSource)
             : base(taskCompletionSource)
         {
+            Trace.TraceInformation($"Constructed {this}");
             DataHandle = dataHandle;
             QuicBuffer = quicBuffer;
         }
 
         public override unsafe void Dispose()
         {
+            Trace.TraceInformation($"Disposing {this}");
             DataHandle.Dispose();
             NativeMemory.Free(QuicBuffer);
         }
+
+        public override unsafe string ToString()
+            => $"[PinnedDataSendContext 0x{(long)(nuint)DataHandle.Pointer:X}]";
     }
 }
